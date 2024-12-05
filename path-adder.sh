baa985e7eb41d36bc85f6db52172f0b700790fde
Nathan Young <75765999+Pizza2d1@users.noreply.github.com>
2024-12-05 15:52:54 -0700

    More features and new file
    --add and --select functions were added, path-adder.sh can be used to allow "pizzapaper" to be used instead of "./pizza-paper.sh"

--- path-adder.sh
+++ path-adder.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #Can be used to copy pizza-paper.sh to one of your PATH folders, mainly so that you don't have to be in a specific directory to use it and I don't like it ending with .sh (github requires it)
 #YOU DO NOT NEED TO RUN THIS, IT IS COMPLETELY **OPTIONAL** AND IS ONLY FOR MILD CONVENIENCE AND DEVELOPMENT
 
@@ -10,12 +11,18 @@ RemoveFile (){
     if [ $? -ne 0 ]; then
       echo "/usr/custom_paths wasn't empty, you either have extra files in that directory or will have do remove it manually"
     fi
+=======
+RemoveFile (){
+  if test -f /usr/local/bin/pizzapaper; then		#If pizzapaper is found in the path file, then it will delete it
+    rm /usr/local/bin/pizzapaper
+>>>>>>> bc8de37 (More features and new file)
   else
     echo "pizzapaper was not found in that directory, it may have already been removed"
   fi
 }
 
 AddFile (){
+<<<<<<< HEAD
   if ! test -f /usr/custom_paths/pizzapaper; then		#If pizzapaper is NOT found in the path file, it will add it
     if test -f ./pizza-paper.sh; then
       mkdir /usr/custom_paths
@@ -41,6 +48,15 @@ ResetSettings (){
 }
 
 options=$(getopt -o ar,add,remove --long "add,remove,retard,copy" -- "$@")
+=======
+  if ! test -f /usr/local/bin/pizzapaper; then		#If pizzapaper is NOT found in the path file, it will add it
+    cp ./pizza-paper.sh /usr/local/bin/pizzapaper
+  else
+    echo "pizzapaper is already in the path directory: /usr/local/bin/"
+  fi
+}
+options=$(getopt -o ar,add,remove --long "add,remove" -- "$@")
+>>>>>>> bc8de37 (More features and new file)
 [ $? -eq 0 ] || { 
     echo "Incorrect options provided"
     exit 1
@@ -54,7 +70,11 @@ while true; do
       -r)
          RemoveFile
          exit;;
+<<<<<<< HEAD
       \?)
+=======
+      \?)				#Doesn't fucking work for some reason, its supposed to detect gibberish like "-klaneofbaog"
+>>>>>>> bc8de37 (More features and new file)
          echo "Invalid option"
          exit;;
     --add)
@@ -65,6 +85,7 @@ while true; do
         shift;
         RemoveFile
         exit;;
+<<<<<<< HEAD
     --retard) #users don't use this, fuck off)
         shift;
         echo "Resetting pizzapaper contents"
@@ -88,6 +109,9 @@ while true; do
         echo "Why would this have a version number, dumbass"
         exit;;
     --)
+=======
+    --)					#No idea whatsoever, I don't want to remove it though
+>>>>>>> bc8de37 (More features and new file)
         shift
         break
         ;;
@@ -99,4 +123,7 @@ echo -e "\nThis lets you add or remove the pizza-paper.sh file to your \$PATH fo
 echo -e "To run this file, you have to provide an argument as either\n"
 echo -e " ./pizza_path_partner -add   		Add the pizza-paper.sh file to /usr/local/bin/ to be able to execute it as \"pizzapaper [ARG]\" instead"
 echo -e " ./pizza_path_partner -remove		Removes the pizzapaper file from your \$PATH folder\n"
+<<<<<<< HEAD
 echo -e "Make sure you are doing this in sudo, as it was made to modify your machine folders which are locked behind admin perms"
+=======
+>>>>>>> bc8de37 (More features and new file)
