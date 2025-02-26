06b66e1470c6f1a70443c00b2638ca660aa20f28
Nathan Young <yyou9481138@gmail.com>
2025-02-25 17:53:37 -0700

    forgot to redo the explanations in the path-adder.sh file because it was pointing towards the wrong save directory

--- path-adder.sh
+++ path-adder.sh
@@ -95,7 +95,7 @@ while true; do
     shift
 done
 
-echo -e "\nThis lets you add or remove the pizza-paper.sh file to your \$PATH folder, specifically the /usr/local/bin/ folder (which is generally empty so it's easy to find)"
+echo -e "\nThis lets you add or remove the pizza-paper.sh file to your \$PATH folder, specifically the /usr/custom_paths folder (which I use for my custom_commands as well)"
 echo -e "To run this file, you have to provide an argument as either\n"
 echo -e " ./pizza_path_partner -add   		Add the pizza-paper.sh file to /usr/local/bin/ to be able to execute it as \"pizzapaper [ARG]\" instead"
 echo -e " ./pizza_path_partner -remove		Removes the pizzapaper file from your \$PATH folder\n"
